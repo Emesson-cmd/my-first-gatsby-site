03707753d49d5b49c7bbba247027f2903ce51be9
Emesson-cmd <emerssosnkavallcante@gmail.com>
2023-10-19 10:08:39 -0300

    feat: add tour demo 03

--- src/pages/tours/index.js
+++ src/pages/tours/index.js
@@ -4,12 +4,13 @@ import Layout from '../../components/layout';
 import Seo from '../../components/seo';
 import TourDemo01 from './demo-01/index';
 import TourDemo02 from './demo-02/index';
+import TourDemo03 from './demo-03/index';
 import { demoContainer, navLinks, navItems, navItemSelected } from './index.module.css';
 
 const TourPage = () => {
   const [selected, setSelected] = React.useState(0);
-  const navLinkItems = ['Demo 01', 'Demo 02'];
-  const demoList = [<TourDemo01 />, <TourDemo02 />];
+  const navLinkItems = ['Demo 01', 'Demo 02', 'Demo 03'];
+  const demoList = [<TourDemo01 />, <TourDemo02 />, <TourDemo03 />];
 
   return (
     <Layout pageTitle="Tour Examples">
