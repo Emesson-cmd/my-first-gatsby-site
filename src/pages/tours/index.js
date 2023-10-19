4b4c57b54bb60c5da9fd4c6c0742ad3e85e8cb61
Emesson-cmd <emerssosnkavallcante@gmail.com>
2023-10-19 14:19:48 -0300

    refactor: removing reactour schemas

--- src/pages/tours/index.js
+++ src/pages/tours/index.js
@@ -1,17 +1,13 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
 import Layout from '../../components/layout';
 import Seo from '../../components/seo';
-import TourDemo01 from './demo-01/index';
-import TourDemo02 from './demo-02/index';
-import TourDemo03 from './demo-03/index';
 import TourDemo04 from './demo-04/index';
 import { demoContainer, navLinks, navItems, navItemSelected } from './index.module.css';
 
 const TourPage = () => {
   const [selected, setSelected] = React.useState(0);
-  const navLinkItems = ['Demo 01', 'Demo 02', 'Demo 03', "Demo 04"];
-  const demoList = [<TourDemo01 />, <TourDemo02 />, <TourDemo03 />,  <TourDemo04 />];
+  const navLinkItems = [ 'Demo 04'];
+  const demoList = [, <TourDemo04 />];
 
   return (
     <Layout pageTitle="Tour Examples">
